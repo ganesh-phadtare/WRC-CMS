bc6315f28c2be85828f3bf1013a1458b5f4d22e5
virendralad <virendra.lad@v2solutions.com>
2017-02-20 18:02:23 +0530

    New added Content Style page/ Add code for logo
    New added Content Style page/ Add code for logo
    
    # Conflicts:
    #	WRC-CMS/WRC-CMS.csproj

--- WRC-CMS/Models/BaseModel.cs
+++ WRC-CMS/Models/BaseModel.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace WRC_CMS.Models
 {
     public class BaseModel
-    {      
+    {
         [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "URL is required.")]
         public string URL { get; set; }
 
-        //[Required(ErrorMessage = "Address is required.")]
-        //public string Logo { get; set; }  
-
-        [Required(ErrorMessage = "Titile is required.")]
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
+
+        //[Column(TypeName = "image")]
+        public byte[] Logo { get; set; }
 
         public bool IsActive { get; set; }
     }
