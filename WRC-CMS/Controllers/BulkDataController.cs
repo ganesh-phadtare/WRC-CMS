b9639233722fa41a0d7ce07bb69729433d519ef4
Ganesh Phadtare <ganesh.phadtare@v2solutions.com>
2017-02-22 16:56:47 +0530

    Revert "Revert "Changes done for""
    This reverts commit 98064859ac52c4c8151cc3bedf0f11f076a82f23.

--- WRC-CMS/Controllers/BulkDataController.cs
+++ WRC-CMS/Controllers/BulkDataController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WRC_CMS.Communication;
 using WRC_CMS.Models;
 using WRC_CMS.Repository;
 
@@ -10,26 +12,38 @@ namespace WRC_CMS.Controllers
 {
     public class BulkDataController : Controller
     {
+        WebApiProxy proxy = new WebApiProxy();
         public ActionResult BulkData()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult CreateBulkRecords(SiteModel SiteObject)
+        public ActionResult BulkData(BulkDataModel bulkData)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
-                    BORepository SiteRepo = new BORepository();
-
-                    if (SiteRepo.AddRecord(SiteObject))
+                    if (bulkData != null)
                     {
+                        //Dictionary<string, object> dict = new Dictionary<string, object>();
+                        //for (int i = 0; i < bulkData.NoOfRecords; i++)
+                        //{
+                        //    dict.Add("@Oid", -1);
+                        //    dict.Add("@Name", MockData.Product.ProductName());
+                        //    dict.Add("@url", MockData.Internet.DomainName());
+                        //    dict.Add("@Logo", 01010101010100000);
+                        //    dict.Add("@Title", "");
+                        //    dict.Add("@IsActive", 1);
+
+                        //    proxy.ExecuteNonQuery("SP_SiteAddUp", dict);
+                        //    dict.Clear();
+                        //}
+
                         ViewBag.Message = "Site added successfully";
                     }
                 }
-
                 return View();
             }
             catch
@@ -37,5 +51,6 @@ namespace WRC_CMS.Controllers
                 return View();
             }
         }
+
     }
 }
