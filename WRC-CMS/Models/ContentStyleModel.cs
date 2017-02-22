deb7e1f2be090388c3e728c7c9dbdbfc7c868141
virendralad <virendra.lad@v2solutions.com>
2017-02-22 18:39:53 +0530

    added changes to disply list view and added Attribute to display rich text editor.
    added changes to disply list view and added Attribute to display rich
    text editor.

--- WRC-CMS/Models/ContentStyleModel.cs
+++ WRC-CMS/Models/ContentStyleModel.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace WRC_CMS.Models
 {
     public class ContentStyleModel
     {
+        public int Oid { get; set; }
+
         [Required(ErrorMessage = "Name is required.")]
         [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Description is required.")]
+        [AllowHtml]
+        [UIHint("tinymce_full")]
         public string Description { get; set; }
 
         public bool IsActive { get; set; }
