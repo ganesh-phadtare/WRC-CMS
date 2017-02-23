75556d85edf97c5b65e03525f0023f6ec428afbf
swapnaliV2 <swapnali.angre@v2solutions.com>
2017-02-23 11:55:33 +0530

    local commit

---
+++ WRC-CMS/Controllers/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WRC_CMS.Models;
+
+namespace WRC_CMS.Controllers
+{
+    public class MenuController : Controller
+    {
+        //
+        // GET: /Menu/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddMenu(SiteModel objSiteModel)
+        {
+            return View();
+        }
+	}
+}
