5db48a2f343c941d3b4330bd4136a3a41c7842d9
virendralad <virendra.lad@v2solutions.com>
2017-03-09 14:23:59 +0530

    Updated SiteMisc as per new structure.
    Updated SiteMisc as per new structure.

--- WRC-CMS/Models/SiteMiscModel.cs
+++ WRC-CMS/Models/SiteMiscModel.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace WRC_CMS.Models
 {
     public class SiteMiscModel
     {
+        public SiteMiscModel()
+        {
+            Keys = new List<string>();
+            if (!ReferenceEquals(WebConfigurationManager.AppSettings["SiteMiscKeys"], null))
+            {
+                string StringKeys = WebConfigurationManager.AppSettings["SiteMiscKeys"];
+                if (!string.IsNullOrEmpty(StringKeys))
+                {
+                    var Split = StringKeys.Split(',');
+                    if (Split.Count() > 0)
+                    {
+                        foreach (var key in Split)
+                        {
+                            Keys.Add(key);
+                        }
+                    }
+                }
+            }
+        }
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Key is required.")]
         [StringLength(50, ErrorMessage = "Key cannot be longer than 50 characters.")]
         public string Key { get; set; }
+
+        [Required(ErrorMessage = "Key is required.")]
+        public List<string> Keys { get; set; }
 
         [Required(ErrorMessage = "Value is required.")]
         [StringLength(250, ErrorMessage = "Value cannot be longer than 250 characters.")]
